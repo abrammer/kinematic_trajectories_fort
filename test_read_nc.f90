047883f533900ad47b155329925a12cf279d40db
Alan Brammer <abrammer@acacia.atmos.albany.edu>
2013-12-10 15:20:32 +0000

    Started to add some error checks - still needs work

--- test_read_nc.f90
+++ test_read_nc.f90
@@ -17,7 +17,7 @@
     character (len = 25)  lat_name, lon_name, lev_name, tim_name, var_name
     integer, parameter :: text_out = 20
     real, dimension(:), allocatable :: lat, lon, lev, time
-    integer ncid, varId, dimId, ndim, dimlen, uId, inds(4), ninds(4),i, numAtts, it
+    integer ncid, varId, dimId, ndim, dimlen, uId, inds(4), ninds(4),i, numAtts, it,x
     integer, dimension(nf90_max_var_dims) :: dimIds
     real ti(3), li(3), loni(3),lati(3), var(4,4,4), pro1(4), val
 	real start_time, start_lon, start_lat, start_lev, end_time
@@ -25,10 +25,9 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!! External Functions
     real bicubic_interp, neville_interp
-
+    integer istat
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!  Lets get Rolling.
-
     type wind
     	real :: val1, val2, val
     	real, dimension(:), allocatable :: lat
@@ -40,7 +39,7 @@
     type (wind) u
     type (wind) v
     type (wind) w
-
+        istat = 1
     call check( nf90_open(FILE_NAME, NF90_NOWRITE, ncid) )
 
     var_name = "XTIME"
@@ -100,9 +99,9 @@
     end function todeg
 
 
-	real function move_parcel(u,v,w,lev_in, lat_in, lon_in)
-	real u,v,w
-	REAL, PARAMETER :: pi = 3.14159265358979  ! pi
+    real function move_parcel(u,v,w,lev_in, lat_in, lon_in)
+    real u,v,w
+    REAL, PARAMETER :: pi = 3.14159265358979  ! pi
     REAL, PARAMETER :: radius = 6371220.0     ! Earth radius (m)
     dimension move_parcel(3)
     REAL :: latr0, lonr0, latr1, lonr1, lat_in, lon_in       ! Initial and guess lat/lon (radians)
@@ -143,8 +142,11 @@
     call cpu_time ( t1 )
     do while (start_time .lt. end_time)
        call petterson(u,v,w, start_time, start_lev, start_lat, start_lon)
-       write(text_out,FMT1) start_time, start_lev, start_lat, start_lon, u%val1, v%val1, w%val1
-10  end do
+       if(.not.istat)then
+		return
+	end if
+	write(text_out,FMT1) start_time, start_lev, start_lat, start_lon, u%val1, v%val1, w%val1
+    end do
     call cpu_time ( t2 )
     write ( *, * ) 'Elapsed CPU time = ', t2 - t1
     end subroutine integrate_trajectory
@@ -171,7 +173,7 @@
     newlocs = move_parcel(0.5*(u0%val1+u%val1),0.5*(v0%val1+v%val1),0.5*(w0%val1+w%val1), lev_in, lat_in, lon_in)
     call get5dval(ncid, u,v,w, in_time, newlocs(1), newlocs(2), newlocs(3) )
     if(maxval(locs - newlocs).eq.0)then ! check for convergence.
-        exit
+    exit
     end if
     end do
     lev_in = newlocs(1)
@@ -232,10 +234,8 @@
 	end subroutine define_coords
 
 
-    real function coord_2_int(x, xx)
-    real x(:), xx, i, int
-    dimension coord_2_int(3)
-
+    subroutine coord_2_int(x, xx, coords)
+    real x(:), xx, int, coords(3)
     do i=1, size(x)-1
        if(x(2) .gt. x(1)) then
             if( xx >= x(i) .and. xx < x(i+1) )then
@@ -249,13 +249,15 @@
             end if
         end if
     end do
-    if( int .le. 2 || int .ge. size(x)-3 )then
-!!!  need to add an istat flag to all these routines so we can quit out.
-    end do
-    coord_2_int(1) = int
-    coord_2_int(2) = i
-    coord_2_int(3) = ( x(i+1) - x(i) )
-    end function coord_2_int
+    if((size(x).gt.3).and. ((i .le. 2) .or. (i .ge. size(x)-3)) )then
+       istat = istat+1
+! add check to inds in get levels maybe instead. 
+! can then check if dimension concurrently.  
+   end if
+    coords(1) = int
+    coords(2) = i
+    coords(3) = ( x(i+1) - x(i) )
+    end subroutine coord_2_int
 
 
 	subroutine get_levels(ncid, u,v,w, in_time, in_lat, in_lon)
@@ -268,7 +270,7 @@
 	integer varId, tId, coords(2), ndim
 	character (len =25) dimname, newname
 	real bicubic_interp, neville_interp
-
+	
 	 call check( nf90_inq_varid(ncid, "PHB", tId ) )
 	 call check( nf90_inquire_variable(ncid, tId, ndims = ndim, dimids = dimIds) )
 	 do i=1,ndim
@@ -278,9 +280,13 @@
 			allocate (pert_levels(4,4,dimlen))
 			allocate (profile(dimlen))
 			define_levels = (define_levels + pert_levels) / g
-			loni =  coord_2_int(w%lon, in_lon)
-   			lati =  coord_2_int(w%lat, in_lat)
-   			inds = int( (/loni(2)-1, lati(2)-1, 1, 2 /) )
+			call coord_2_int(w%lon, in_lon,loni)
+   			call coord_2_int(w%lat, in_lat, lati)
+   			if(.not.istat)then
+				print*, istat
+				return
+			end if
+			inds = int( (/loni(2)-1, lati(2)-1, 1, 2 /) )
 			call check (nf90_get_var(ncid, tId, define_levels, inds, (/ 4,4,dimlen,1/) ) )
 			call check( nf90_inq_varid(ncid, "PH", tId ) )
 			call check (nf90_get_var(ncid, tId, pert_levels, inds, (/ 4,4,dimlen,1/) ) )
@@ -302,7 +308,7 @@
 	real in_time, lev, lat, lon
 	integer ncid
 	real linear_interp
-	ti =  coord_2_int(time, in_time)
+	call coord_2_int(time, in_time, ti)
 	call get_levels(ncid, u,v,w, time(ti(2)), lat, lon)
 	u%val1 = get4dval(ncid, u, time(ti(2)), lev, lat, lon)
 	v%val1 = get4dval(ncid, v, time(ti(2)),lev, lat, lon)
@@ -327,10 +333,13 @@
 	real bicubic_interp, neville_interp
 	ninds = (/4,4,4,1/)
 
-    ti =  coord_2_int(time, in_time)
-    levi =  coord_2_int(reqvar%lev, in_lev)
-    loni =  coord_2_int(reqvar%lon, in_lon)
-    lati =  coord_2_int(reqvar%lat, in_lat)
+    call coord_2_int(time, in_time, ti)
+    call coord_2_int(reqvar%lev, in_lev,levi)
+    call coord_2_int(reqvar%lon, in_lon,lati)
+    call coord_2_int(reqvar%lat, in_lat,loni)
+    if(.not.istat)then
+	return
+    end if
     inds = int( (/loni(2)-1, lati(2)-1, levi(2)-1, ti(2) /) )
 
     call check (nf90_get_var(ncid, reqvar%id, var, inds, ninds ) )
