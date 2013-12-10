c48c0433c46fc23ce9ad92a31f6723e7e1e2f7fd
Alan Brammer <abrammer@yslogin2.(none)>
2013-12-10 11:45:35 -0700

    cleaned up a lot of bugs, added multi file ability

--- test_read_nc.f90
+++ test_read_nc.f90
@@ -1,24 +1,27 @@
-  subroutine check(status)
-  use netcdf
-    integer, intent ( in) :: status
-    if(status /= nf90_noerr) then
-      print *, trim(nf90_strerror(status))
-      stop "Stopped"
-    end if
-  end subroutine check
+     subroutine check(status)
+     use netcdf
+       integer, intent ( in) :: status
+       if(status /= nf90_noerr) then
+         print *, trim(nf90_strerror(status))
+         stop "Stopped"
+       end if
+     end subroutine check
 
 
     PROGRAM read_netcdf
     USE netcdf
     IMPLICIT NONE
-!    real lev(:)
-    character (len = *), parameter :: FILE_NAME = "/free4/abrammer/WRFOUT/10d50km/wrfout_d01_2010-07-05_00:00:00"
+    character (len = *), parameter :: VAR_FILE = "/glade/scratch/abrammer/runs/nadine_2012/run3/wind_d03_2012-09-06_00:00:00.nc"
+    character (len = *), parameter :: META_FILE = "/glade/scratch/abrammer/runs/nadine_2012/run3/wrfout_d03_2012-09-06_00:00:00.nc"
+
     character (len=*), parameter :: FMT1 = "(7F15.5)", FMT0="(7A15)"
     character (len = 25)  lat_name, lon_name, lev_name, tim_name, var_name
-    integer, parameter :: text_out = 20
     real, dimension(:), allocatable :: lat, lon, lev, time
-    integer ncid, varId, dimId, ndim, dimlen, uId, inds(4), ninds(4),i, numAtts, it,x
+
+    integer, parameter :: text_out = 20
+    integer  meta_ncid, ncid, varId, dimId, ndim, dimlen, uId, inds(4), ninds(4),i, numAtts, it,x,t, step
     integer, dimension(nf90_max_var_dims) :: dimIds
+
     real ti(3), li(3), loni(3),lati(3), var(4,4,4), pro1(4), val
 	real start_time, start_lon, start_lat, start_lev, end_time
 	real t2, t1
@@ -39,101 +42,117 @@
     type (wind) u
     type (wind) v
     type (wind) w
-        istat = 1
-    call check( nf90_open(FILE_NAME, NF90_NOWRITE, ncid) )
 
-    var_name = "XTIME"
-    call check( nf90_inq_varid(ncid, var_name, varId ) )
-    call check( nf90_inquire_variable(ncid, varId, ndims = ndim, dimids = dimIds, natts = numAtts) )
-    call check( nf90_inquire_variable(ncid, varId, dimids = dimIds(:ndim) ) )
-    call check( nf90_inquire_dimension(ncid, dimids(1), len=dimlen) )
-    allocate ( time(dimlen) )
-    call check (nf90_get_var(ncid, varId, time) )
+    istat = 1
+    call check( nf90_open(VAR_FILE, NF90_NOWRITE, ncid) )
+    call check( nf90_open(META_FILE, NF90_NOWRITE, meta_ncid) )
+
+    !var_name = "XTIME"
+    !call check( nf90_inq_varid(ncid, var_name, varId ) )
+    !call check( nf90_inquire_variable(ncid, varId, ndims = ndim, dimids = dimIds, natts = numAtts) )
+    !call check( nf90_inquire_variable(ncid, varId, dimids = dimIds(:ndim) ) )
+    !call check( nf90_inquire_dimension(ncid, dimids(1), len=dimlen) )
+    !allocate ( time(dimlen) )
+    !call check (nf90_get_var(ncid, varId, time) )
+
+     allocate(time(12))
+     do t=1,12
+        time(t) = (t*5)-5
+        end do
 
 !    ***********************************
 !    define dimensions
 
 	u%name = "U"
 	call define_coords(u)
-	print*, size(u%lon), size(u%lat),u%y_stag
-	print*, maxval(u%lon), maxval(u%lat)
-	print*, minval(u%lon), minval(u%lat)
+	print*, "U Variable"
+	print*, "Longitude  (", size(u%lon),"): ",minval(u%lon),":",maxval(u%lon)
+	print*, "Latitude  (", size(u%lat),"): ",minval(u%lat),":",maxval(u%lat)
 
 	v%name = "V"
 	call define_coords(v)
-	print*, size(v%lon), size(v%lat),v%y_stag
-	print*, maxval(v%lon), maxval(v%lat)
-	print*, minval(v%lon), minval(v%lat)
+	print*, "V Variable"
+	print*, "Longitude  (", size(v%lon),"): ",minval(v%lon),":",maxval(v%lon)
+	print*, "Latitude  (", size(v%lat),"): ",minval(v%lat),":",maxval(v%lat)
 
 	w%name = "W"
 	call define_coords(w)
-	print*, size(w%lon), size(w%lat), w%y_stag
-	print*, maxval(w%lon), maxval(w%lat)
-	print*, minval(w%lon), minval(w%lat)
+	print*, "W Variable"
+	print*, "Longitude  (", size(w%lon),"): ",minval(w%lon),":",maxval(w%lon)
+	print*, "Latitude  (", size(w%lat),"): ",minval(w%lat),":",maxval(w%lat)
 
 
 !    ***********************************
 !   starting point
 
-    start_time = 170.
-    end_time = 190.
-    start_lon = -65.3
+    start_time = 10.
+    end_time =  50.
+    start_lon = -25.3
     start_lat = 15.2
-    start_lev = 15431.0
-
+    start_lev = 1531.0
+    step = 60
 
     call integrate_trajectory(1, start_time, end_time, start_lev, start_lat, start_lon)
 
+
 	contains
 
-	REAL function torad(val)
-	REAL, PARAMETER :: pi = 3.14159265358979  ! pi
-	real val
-    torad = val* pi/180.
-    end function torad
-
-    REAL function todeg(val)
-	REAL, PARAMETER :: pi = 3.14159265358979  ! pi
-	real val
-    todeg = val* 180./pi
-    end function todeg
-
-
-    real function move_parcel(u,v,w,lev_in, lat_in, lon_in)
-    real u,v,w
-    REAL, PARAMETER :: pi = 3.14159265358979  ! pi
-    REAL, PARAMETER :: radius = 6371220.0     ! Earth radius (m)
-    dimension move_parcel(3)
-    REAL :: latr0, lonr0, latr1, lonr1, lat_in, lon_in       ! Initial and guess lat/lon (radians)
-    REAL :: dir, rdist, adj , newlat, newlon, time_step, newlev, lev_in          ! Direction, radial distance, backward/forward switch
-
-    time_step = 30
-    adj = 1.
-
-	rdist = SQRT( u**2.0+v**2.0 )*(adj*time_step/radius)      ! is adj needed?
-    dir = pi+ATAN2( adj*-1.0*u, adj*-1.0*v )
-
-    latr0 = torad(lat_in)
-    lonr0 = torad(lon_in)
-
-    ! Calculate new lat and lon using Haversine's forumula
-    latr1 = ASIN( SIN(latr0)*COS(rdist)+COS(latr0)*SIN(rdist)*COS(dir) )
-    lonr1 = lonr0+ATAN2( SIN(dir)*SIN(rdist)*COS(latr0), COS(rdist)-SIN(latr0)*SIN(latr1) )
-
-    newlev = lev_in + (adj*time_step)*w
-
-    ! Convert to degrees
-    newlat = todeg(latr1)
-    newlon = todeg(lonr1)
-    move_parcel = (/newlev, newlat, newlon/)
-    end function move_parcel
-
+	subroutine define_coords(vari)
+	type (wind) vari
+	integer varId, tId
+	character (len =25) dimname, newname
+	real, dimension(:), allocatable :: lonvar, latvar
+    call check( nf90_inq_varid(ncid, vari%name, vari%id ) )
+    call check( nf90_inquire_variable(ncid, vari%id, ndims = ndim, dimids = dimIds, natts = numAtts) )
+    call check( nf90_inquire_variable(ncid, vari%id, dimids = dimIds(:ndim) ) )
+    do i=1,ndim
+		call check( nf90_inquire_dimension(ncid, dimids(i),name = dimname, len=dimlen) )
+		if(dimname == "west_east")then
+			allocate ( vari%lon(dimlen) )
+			allocate ( lonvar(dimlen) )
+			call check( nf90_inq_varid(meta_ncid, "XLONG", tId) )
+			call check (nf90_get_var(meta_ncid, tId, lonvar, (/1,1,1,1/), (/dimlen,1,1,0/) ) )
+			vari%lon = lonvar
+		end if
+		if(dimname == "west_east_stag")then
+			allocate ( vari%lon(dimlen) )
+			allocate ( lonvar(dimlen) )
+			call check( nf90_inq_varid(meta_ncid, "XLONG_U", tId ) )
+			call check (nf90_get_var(meta_ncid, tId, lonvar, (/1,1,1,1/), (/dimlen,1,1,0/) ) )
+			vari%lon = lonvar
+		end if
+		if(dimname == "south_north")then
+			allocate ( vari%lat(dimlen) )
+			allocate ( latvar(dimlen) )
+			call check( nf90_inq_varid(meta_ncid, "XLAT", tId ) )
+			call check (nf90_get_var(meta_ncid, tId,latvar, (/1,1,1,1/), (/ 1,dimlen,1,0/) ) )
+			vari%lat = latvar
+		end if
+		if(dimname == "south_north_stag")then
+			allocate ( vari%lat(dimlen) )
+			allocate ( latvar(dimlen) )
+			call check( nf90_inq_varid(meta_ncid, "XLAT_V", tId ) )
+			call check (nf90_get_var(meta_ncid, tId,latvar, (/1,1,1,1/), (/ 1,dimlen,1,0/) ) )
+			vari%lat = latvar
+		end if
+		if(dimname == "bottom_top")then
+            allocate (vari%lev(dimlen))
+            vari%y_stag = 0
+		end if
+		if(dimname == "bottom_top_stag")then
+            vari%y_stag = 1
+            allocate (vari%lev(dimlen))
+		end if
+    end do
+	return
+	end subroutine define_coords
 
     subroutine integrate_trajectory(tr, start_time, end_time, start_lev, start_lat, start_lon)
     real start_time, end_time, start_lev, start_lat, start_lon
     integer tr
-
-    open(unit=text_out, file="traj1.txt", status="replace", action="write")
+    character (len=3) :: stri
+    write(stri,'(I3)') tr
+    open(unit=text_out, file="traj"//trim(ADJUSTL(stri))//".txt", status="replace", action="write")
     write(text_out,FMT0) "TIME","LEV","LAT","LON","U","V","W"
 
 	call get5dval(ncid, u,v,w, start_time, start_lev, start_lat, start_lon)
@@ -145,12 +164,13 @@
        if(.not.istat)then
 		return
 	end if
+    if( mod(start_time, 5.).eq.0 )then
 	write(text_out,FMT1) start_time, start_lev, start_lat, start_lon, u%val1, v%val1, w%val1
+    end if
     end do
     call cpu_time ( t2 )
     write ( *, * ) 'Elapsed CPU time = ', t2 - t1
     end subroutine integrate_trajectory
-
 
     subroutine petterson(u,v,w,in_time,lev_in, lat_in, lon_in)
     type (wind) :: u,v,w, u0, v0, w0
@@ -162,11 +182,11 @@
 	! iterate with updated new vector until convergent.
     u0 = u
     v0 = v
-    w0 = v
+    w0 = w
 
     newlocs = move_parcel(u%val1,v%val1,w%val1, lev_in, lat_in, lon_in)
-    in_time = in_time + 0.5
-    call get5dval(ncid, u,v,w, in_time, newlocs(1), newlocs(2), newlocs(3) )  ! get initial guess
+    in_time = in_time + step/60.
+    call get5dval(ncid,u,v,w, in_time, newlocs(1), newlocs(2), newlocs(3) )  ! get initial guess
 
     do it=1, 3
     locs  = newlocs
@@ -182,133 +202,55 @@
     end subroutine petterson
 
 
+    real function move_parcel(u,v,w,lev_in, lat_in, lon_in)
+    real u,v,w
+    REAL, PARAMETER :: pi = 3.14159265358979  ! pi
+    REAL, PARAMETER :: radius = 6371220.0     ! Earth radius (m)
+    dimension move_parcel(3)
+    REAL :: latr0, lonr0, latr1, lonr1, lat_in, lon_in       ! Initial and guess lat/lon (radians)
+    REAL :: dir, rdist, adj , newlat, newlon, time_step, newlev, lev_in          ! Direction, radial distance, backward/forward switch
 
-	subroutine define_coords(vari)
-	type (wind) vari
-	integer varId, tId
-	character (len =25) dimname, newname
-	real, dimension(:), allocatable :: lonvar, latvar
-    call check( nf90_inq_varid(ncid, vari%name, vari%id ) )
-    call check( nf90_inquire_variable(ncid, vari%id, ndims = ndim, dimids = dimIds, natts = numAtts) )
-    call check( nf90_inquire_variable(ncid, vari%id, dimids = dimIds(:ndim) ) )
-    do i=1,ndim
-		call check( nf90_inquire_dimension(ncid, dimids(i),name = dimname, len=dimlen) )
-		if(dimname == "west_east")then
-			allocate ( vari%lon(dimlen) )
-			allocate ( lonvar(dimlen) )
-			call check( nf90_inq_varid(ncid, "XLONG", tId) )
-			call check (nf90_get_var(ncid, tId, lonvar, (/1,1,1,1/), (/dimlen,1,1,0/) ) )
-			vari%lon = lonvar
-		end if
-		if(dimname == "west_east_stag")then
-			allocate ( vari%lon(dimlen) )
-			allocate ( lonvar(dimlen) )
-			call check( nf90_inq_varid(ncid, "XLONG_U", tId ) )
-			call check (nf90_get_var(ncid, tId, lonvar, (/1,1,1,1/), (/dimlen,1,1,0/) ) )
-			vari%lon = lonvar
-		end if
-		if(dimname == "south_north")then
-			allocate ( vari%lat(dimlen) )
-			allocate ( latvar(dimlen) )
-			call check( nf90_inq_varid(ncid, "XLAT", tId ) )
-			call check (nf90_get_var(ncid, tId,latvar, (/1,1,1,1/), (/ 1,dimlen,1,0/) ) )
-			vari%lat = latvar
-		end if
-		if(dimname == "south_north_stag")then
-			allocate ( vari%lat(dimlen) )
-			allocate ( latvar(dimlen) )
-			call check( nf90_inq_varid(ncid, "XLAT_V", tId ) )
-			call check (nf90_get_var(ncid, tId,latvar, (/1,1,1,1/), (/ 1,dimlen,1,0/) ) )
-			vari%lat = latvar
-		end if
-		if(dimname == "bottom_top")then
-		 allocate (vari%lev(dimlen))
-		vari%y_stag = 0
-		end if
-		if(dimname == "bottom_top_stag")then
-		vari%y_stag = 1
-		allocate (vari%lev(dimlen))
-		end if
-    end do
-	return
-	end subroutine define_coords
+    time_step = step
+    adj = 1.
+
+	rdist = SQRT( u**2.0+v**2.0 )*(adj*time_step/radius)      ! is adj needed?
+    dir = pi+ATAN2( adj*-1.0*u, adj*-1.0*v )
+
+    latr0 = torad(lat_in)
+    lonr0 = torad(lon_in)
+
+    ! Calculate new lat and lon using Haversine's forumula
+    latr1 = ASIN( SIN(latr0)*COS(rdist)+COS(latr0)*SIN(rdist)*COS(dir) )
+    lonr1 = lonr0+ATAN2( SIN(dir)*SIN(rdist)*COS(latr0), COS(rdist)-SIN(latr0)*SIN(latr1) )
+
+    newlev = lev_in + (adj*time_step)*w
+    ! Convert to degrees
+    newlat = todeg(latr1)
+    newlon = todeg(lonr1)
+    move_parcel = (/newlev, newlat, newlon/)
+    end function move_parcel
 
 
-    subroutine coord_2_int(x, xx, coords)
-    real x(:), xx, int, coords(3)
-    do i=1, size(x)-1
-       if(x(2) .gt. x(1)) then
-            if( xx >= x(i) .and. xx < x(i+1) )then
-            int = 1.0* (xx - x(i)) / (x(i+1) - x(i) )
-            exit
-            end if
-        else
-            if( xx <= x(i) .and. xx > x(i+1) )then
-            int = 1.0* (xx - x(i)) / (x(i+1) - x(i) )
-            exit
-            end if
-        end if
-    end do
-    if((size(x).gt.3).and. ((i .le. 2) .or. (i .ge. size(x)-3)) )then
-       istat = istat+1
-! add check to inds in get levels maybe instead. 
-! can then check if dimension concurrently.  
-   end if
-    coords(1) = int
-    coords(2) = i
-    coords(3) = ( x(i+1) - x(i) )
-    end subroutine coord_2_int
+	REAL function torad(val)
+	REAL, PARAMETER :: pi = 3.14159265358979  ! pi
+	real val
+    torad = val* pi/180.
+    end function torad
 
+    REAL function todeg(val)
+	REAL, PARAMETER :: pi = 3.14159265358979  ! pi
+	real val
+    todeg = val* 180./pi
+    end function todeg
 
-	subroutine get_levels(ncid, u,v,w, in_time, in_lat, in_lon)
-	real, parameter :: g=9.81
-	type (wind) :: u,v,w
-	real loni(3), lati(3),  in_lat, in_lon, in_time
-	real, dimension(:,:,:), allocatable :: define_levels, pert_levels
-	real, dimension(:), allocatable :: profile, ustag_lev
-	integer inds(4), ncid, l, i
-	integer varId, tId, coords(2), ndim
-	character (len =25) dimname, newname
-	real bicubic_interp, neville_interp
-	
-	 call check( nf90_inq_varid(ncid, "PHB", tId ) )
-	 call check( nf90_inquire_variable(ncid, tId, ndims = ndim, dimids = dimIds) )
-	 do i=1,ndim
-		 call check( nf90_inquire_dimension(ncid, dimids(i),name = dimname, len=dimlen) )
-		 if(dimname == "bottom_top_stag")then
-			allocate (define_levels(4,4,dimlen))
-			allocate (pert_levels(4,4,dimlen))
-			allocate (profile(dimlen))
-			define_levels = (define_levels + pert_levels) / g
-			call coord_2_int(w%lon, in_lon,loni)
-   			call coord_2_int(w%lat, in_lat, lati)
-   			if(.not.istat)then
-				print*, istat
-				return
-			end if
-			inds = int( (/loni(2)-1, lati(2)-1, 1, 2 /) )
-			call check (nf90_get_var(ncid, tId, define_levels, inds, (/ 4,4,dimlen,1/) ) )
-			call check( nf90_inq_varid(ncid, "PH", tId ) )
-			call check (nf90_get_var(ncid, tId, pert_levels, inds, (/ 4,4,dimlen,1/) ) )
-		   do l=1,dimlen
-		   profile(l) = bicubic_interp(define_levels(:,:,l), lati(1), loni(1) )
-		   end do
-		   w%lev = profile
-			exit
-		end if
-	 end do
-	 allocate (ustag_lev(dimlen-1))
-	 ustag_lev =  (profile(2:dimlen) + profile(1:dimlen-1) )/ 2.
-	 u%lev = ustag_lev
-	 v%lev = ustag_lev
-    end subroutine get_levels
 
 	subroutine get5dval(ncid, u,v,w, in_time, lev, lat, lon)
 	type (wind) u,v,w
-	real in_time, lev, lat, lon
+	real in_time, lev, lat, lon, ti(3)
 	integer ncid
 	real linear_interp
 	call coord_2_int(time, in_time, ti)
+
 	call get_levels(ncid, u,v,w, time(ti(2)), lat, lon)
 	u%val1 = get4dval(ncid, u, time(ti(2)), lev, lat, lon)
 	v%val1 = get4dval(ncid, v, time(ti(2)),lev, lat, lon)
@@ -323,6 +265,55 @@
 	  w%val1 = linear_interp( w%val1, w%val2, ti(1) )
 	end if
 	end subroutine get5dval
+
+	subroutine get_levels(ncid, u,v,w, in_time, in_lat, in_lon)
+	real, parameter :: g=9.81
+	type (wind) :: u,v,w
+	real loni(3), lati(3),  in_lat, in_lon, in_time
+	real, dimension(:,:,:), allocatable :: define_levels, pert_levels
+	real, dimension(:), allocatable :: profile, ustag_lev
+	integer inds(4), ncid, l, i
+	integer varId, tId, coords(2), ndim
+	character (len =25) dimname, newname
+	real bicubic_interp, neville_interp
+	 call check( nf90_inq_varid(meta_ncid, "PHB", tId ) )
+	 call check( nf90_inquire_variable(meta_ncid, tId, ndims = ndim, dimids = dimIds) )
+	 do i=1,ndim
+		 call check( nf90_inquire_dimension(meta_ncid, dimids(i),name = dimname, len=dimlen) )
+		 if(dimname == "bottom_top_stag")then
+			allocate (define_levels(4,4,dimlen))
+			allocate (pert_levels(4,4,dimlen))
+			allocate (profile(dimlen))
+			call coord_2_int(w%lon, in_lon,loni)
+   			call coord_2_int(w%lat, in_lat, lati)
+   			call coord_2_int(time, in_time, ti)
+
+   			if(.not.istat)then
+				print*, istat, "Error in get_levels"
+				print*, loni, lati
+				return
+			end if
+
+			inds = int( (/loni(2)-1, lati(2)-1, 1, 1 /) )  ! meta time ind should always be one. Constant field so doesn't matter
+			call check (nf90_get_var(meta_ncid, tId, define_levels, inds, (/ 4,4,dimlen,1/) ) )
+
+			inds = int( (/loni(2)-1, lati(2)-1, 1, ti(2) /) )
+			call check( nf90_inq_varid(ncid, "PH", tId ) )
+			call check (nf90_get_var(ncid, tId, pert_levels, inds, (/ 4,4,dimlen,1/) ) )
+
+		   define_levels = (define_levels + pert_levels) / g
+		   do l=1,dimlen
+		   profile(l) = bicubic_interp(define_levels(:,:,l), lati(1), loni(1) )
+		   end do
+		   w%lev = profile
+			exit
+		end if
+	 end do
+	 allocate (ustag_lev(dimlen-1))
+	 ustag_lev =  (profile(2:dimlen) + profile(1:dimlen-1) )/ 2.
+	 u%lev = ustag_lev
+	 v%lev = ustag_lev
+    end subroutine get_levels
 
 
 	real function get4dval(ncid, reqvar, in_time, in_lev, in_lat, in_lon)
@@ -352,11 +343,42 @@
 
 
 	if(ti(1).ne.0)then
-!   would also be a istat time. 
+!   would also be a istat time.
 	end if
 
     get4dval = val
     end function get4dval
+
+
+
+    subroutine coord_2_int(x, xx, coords)
+    real x(:), xx, int, coords(3)
+    do i=1, size(x)-1
+       if(x(2) .gt. x(1)) then
+            if( xx >= x(i) .and. xx < x(i+1) )then
+            int = 1.0* (xx - x(i)) / (x(i+1) - x(i) )
+            exit
+            end if
+        else
+            if( xx <= x(i) .and. xx > x(i+1) )then
+            int = 1.0* (xx - x(i)) / (x(i+1) - x(i) )
+            exit
+            end if
+        end if
+    end do
+!    if(.and. ((i .le. 2) .or. (i .ge. size(x)-3)) )then
+!       istat = istat+1
+! add check to inds in get levels maybe instead.
+! can then check if dimension concurrently.
+!   end if
+    coords(1) = int
+    coords(2) = i
+    coords(3) = ( x(i+1) - x(i) )
+    end subroutine coord_2_int
+
+
+
+
 
 
 	end PROGRAM
