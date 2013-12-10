75566abc2b50026ef2b985bf1c0a673489d06c27
Alan Brammer <abrammer@yslogin4.(none)>
2013-12-10 14:59:21 -0700

    Changed file i/o use, multiple trajectories should now be as fast as single traj

--- test_read_nc.f90
+++ test_read_nc.f90
@@ -29,6 +29,7 @@
 	namelist /time_opt/start_time,end_time,step
 	namelist /traj_opt/start_lev,start_lat,start_lon
 
+    real get_levels_time, get4dal_time
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!! External Functions
     real bicubic_interp, neville_interp
@@ -41,14 +42,19 @@
     	real, dimension(:), allocatable :: lon
     	real, dimension(:), allocatable :: lev
     	real, dimension(:,:,:,:), allocatable :: grid
+    	real, dimension(:,:,:), allocatable :: base
     	character (len = 25):: units, name
-    	integer :: id, y_stag
+    	integer :: id, y_stag, ti
     end type wind
     type (wind) u
     type (wind) v
     type (wind) w
+    type (wind) ph
 
     istat = 1
+    get_levels_time = 0.
+    get4dal_time = 0.
+
     call check( nf90_open(VAR_FILE, NF90_NOWRITE, ncid) )
     call check( nf90_open(META_FILE, NF90_NOWRITE, meta_ncid) )
 
@@ -86,6 +92,12 @@
 	print*, "Longitude  (", size(w%lon),"): ",minval(w%lon),":",maxval(w%lon)
 	print*, "Latitude  (", size(w%lat),"): ",minval(w%lat),":",maxval(w%lat)
 
+	ph%name = "PH"
+	call define_coords(ph)
+	print*, "PH Variable"
+	print*, "Longitude  (", size(w%lon),"): ",minval(w%lon),":",maxval(w%lon)
+	print*, "Latitude  (", size(w%lat),"): ",minval(w%lat),":",maxval(w%lat)
+
 
 
 
@@ -97,8 +109,11 @@
 
 
     call cpu_time ( t1 )
-    call read_time()
+    call grab_grid(u, 3)
+    call grab_grid(v, 3)
+    call grab_grid(w, 3)
     call cpu_time ( t2 )
+
     write ( *, * ) 'Elapsed CPU time = ', t2 - t1
     print*, "*********************"
 
@@ -109,12 +124,11 @@
 
 	contains
 
-	subroutine read_time()
-    use netcdf
-    integer tId
-    real vara(2226, 1125, 51)
-	call check( nf90_inq_varid(ncid, "PH", tId ) )
-	call check (nf90_get_var(ncid, tId, vara, (/1,1,1,1/), (/ 2226,1125,51,1/) ) )
+	subroutine grab_grid(reqvar, ti)
+    type(wind) reqvar
+    integer ti
+	call check (nf90_get_var(ncid, reqvar%id, reqvar%grid, (/1,1,1,ti/), (/ size(reqvar%lon),size(reqvar%lat),size(reqvar%lev),2/) ) )
+	    reqvar%ti = ti
     end subroutine
 
 
@@ -165,6 +179,14 @@
             allocate (vari%lev(dimlen))
 		end if
     end do
+    allocate(vari%grid( size(vari%lon), size(vari%lat), size(vari%lev),2 ) )
+    if(vari%name =="PH")then
+        allocate(vari%base( size(vari%lon), size(vari%lat), size(vari%lev) ) )
+	    call check( nf90_inq_varid(meta_ncid, "PHB", tId ) )
+		call check (nf90_get_var(meta_ncid, tId, vari%base, (/1,1,1,1/), (/ size(vari%lon), size(vari%lat), size(vari%lev),1/) ) )
+    else
+       allocate(vari%base(1,1,1)  )  !! just so we don't have unallocated grids? Not sure whether this matters
+    end if
 	return
 	end subroutine define_coords
 
@@ -176,14 +198,13 @@
     open(unit=text_out, file="traj"//trim(ADJUSTL(stri))//".txt", status="replace", action="write")
     write(text_out,FMT0) "TIME","LEV","LAT","LON","U","V","W"
 
-	call get5dval(ncid, u,v,w, start_time, start_lev, start_lat, start_lon)
+	call get5dval(u,v,w, start_time, start_lev, start_lat, start_lon)
     write(text_out,FMT1) start_time, start_lev, start_lat, start_lon, u%val1, v%val1, w%val1
 
-    do while (start_time .lt. end_time)
     call cpu_time ( t1 )
+    do while (start_time .lt. end_time)
        call petterson(u,v,w, start_time, start_lev, start_lat, start_lon)
-    call cpu_time ( t2 )
-    write ( *, * ) 'Elapsed CPU time = ', t2 - t1
+       print*, get_levels_time, get4dal_time
        if(.not.istat)then
 		return
 	end if
@@ -191,28 +212,34 @@
 	write(text_out,FMT1) start_time, start_lev, start_lat, start_lon, u%val1, v%val1, w%val1
     end if
     end do
+    call cpu_time ( t2 )
+    write ( *, * ) 'Elapsed CPU time = ', t2 - t1
+
     end subroutine integrate_trajectory
 
+
+
     subroutine petterson(u,v,w,in_time,lev_in, lat_in, lon_in)
-    type (wind) :: u,v,w, u0, v0, w0
+    type (wind) :: u,v,w
     real locs(3), newlocs(3), lev_in, lat_in, lon_in, in_time
+    real u0,v0,w0
     integer it
 	! move parcel based on initial vector
 	! Get winds at new location.
 	! move parcel based on avg of initial and new vector
 	! iterate with updated new vector until convergent.
-    u0 = u
-    v0 = v
-    w0 = w
+    u0 = u%val1
+    v0 = v%val1
+    w0 = w%val1
 
     newlocs = move_parcel(u%val1,v%val1,w%val1, lev_in, lat_in, lon_in)
     in_time = in_time + step/60.
-    call get5dval(ncid,u,v,w, in_time, newlocs(1), newlocs(2), newlocs(3) )  ! get initial guess
+    call get5dval(u,v,w, in_time, newlocs(1), newlocs(2), newlocs(3) )  ! get initial guess
 
     do it=1, 3
     locs  = newlocs
-    newlocs = move_parcel(0.5*(u0%val1+u%val1),0.5*(v0%val1+v%val1),0.5*(w0%val1+w%val1), lev_in, lat_in, lon_in)
-    call get5dval(ncid, u,v,w, in_time, newlocs(1), newlocs(2), newlocs(3) )
+    newlocs = move_parcel(0.5*(u0+u%val1),0.5*(v0+v%val1),0.5*(w0+w%val1), lev_in, lat_in, lon_in)
+    call get5dval(u,v,w, in_time, newlocs(1), newlocs(2), newlocs(3) )
     if(maxval(locs - newlocs).eq.0)then ! check for convergence.
     exit
     end if
@@ -265,96 +292,89 @@
     end function todeg
 
 
-	subroutine get5dval(ncid, u,v,w, in_time, lev, lat, lon)
+	subroutine get5dval(u,v,w, in_time, lev, lat, lon)
 	type (wind) u,v,w
-	real in_time, lev, lat, lon, ti(3)
-	integer ncid
+	real in_time, lev, lat, lon, ti(3), t1, t2
 	real linear_interp
 	call coord_2_int(time, in_time, ti)
 
-	call get_levels(ncid, u,v,w, time(ti(2)), lat, lon)
-	u%val1 = get4dval(ncid, u, time(ti(2)), lev, lat, lon)
-	v%val1 = get4dval(ncid, v, time(ti(2)),lev, lat, lon)
-	w%val1 = get4dval(ncid, w, time(ti(2)), lev, lat, lon)
+	call get_levels(u,v,w, time(ti(2)), lat, lon)
+	u%val1 = get4dval(u, time(ti(2)), lev, lat, lon)
+	v%val1 = get4dval(v, time(ti(2)),lev, lat, lon)
+	w%val1 = get4dval(w, time(ti(2)), lev, lat, lon)
 	if(ti(1).ne.0)then
-	  call get_levels(ncid, u,v,w, time(ti(2)+1), lat, lon)
-	  u%val2 = get4dval(ncid, u, time(ti(2)+1),lev, lat, lon)
-	  v%val2 = get4dval(ncid, v, time(ti(2)+1),lev, lat, lon)
-	  w%val2 = get4dval(ncid, w, time(ti(2)+1),lev, lat, lon)
+	  call get_levels(u,v,w, time(ti(2)+1), lat, lon)
+	  u%val2 = get4dval(u, time(ti(2)+1),lev, lat, lon)
+	  v%val2 = get4dval(v, time(ti(2)+1),lev, lat, lon)
+	  w%val2 = get4dval(w, time(ti(2)+1),lev, lat, lon)
 	  u%val1 = linear_interp( u%val1, u%val2, ti(1) )
 	  v%val1 = linear_interp( v%val1, v%val2, ti(1) )
 	  w%val1 = linear_interp( w%val1, w%val2, ti(1) )
 	end if
 	end subroutine get5dval
 
-	subroutine get_levels(ncid, u,v,w, in_time, in_lat, in_lon)
+	subroutine get_levels(u,v,w, in_time, in_lat, in_lon)
 	real, parameter :: g=9.81
 	type (wind) :: u,v,w
-	real loni(3), lati(3),  in_lat, in_lon, in_time
-	real, dimension(:,:,:), allocatable :: define_levels, pert_levels
-	real, dimension(:), allocatable :: profile, ustag_lev
-	integer inds(4), ncid, l, i
+	real loni(3), lati(3),  in_lat, in_lon, in_time, t2, t1
+	real :: define_levels(4,4,51)
+	real :: profile(51), ustag_lev(50)
+	integer inds(4), l, i, tt
 	integer varId, tId, coords(2), ndim
 	character (len =25) dimname, newname
 	real bicubic_interp, neville_interp
-	 call check( nf90_inq_varid(meta_ncid, "PHB", tId ) )
-	 call check( nf90_inquire_variable(meta_ncid, tId, ndims = ndim, dimids = dimIds) )
-	 do i=1,ndim
-		 call check( nf90_inquire_dimension(meta_ncid, dimids(i),name = dimname, len=dimlen) )
-		 if(dimname == "bottom_top_stag")then
-			allocate (define_levels(4,4,dimlen))
-			allocate (pert_levels(4,4,dimlen))
-			allocate (profile(dimlen))
-			call coord_2_int(w%lon, in_lon,loni)
-   			call coord_2_int(w%lat, in_lat, lati)
-   			call coord_2_int(time, in_time, ti)
+    call cpu_time ( t1 )
 
-   			if(.not.istat)then
-				print*, istat, "Error in get_levels"
-				print*, loni, lati
-				return
-			end if
 
-			inds = int( (/loni(2)-1, lati(2)-1, 1, 1 /) )  ! meta time ind should always be one. Constant field so doesn't matter
-			call check (nf90_get_var(meta_ncid, tId, define_levels, inds, (/ 4,4,dimlen,1/) ) )
+     call coord_2_int(ph%lon, in_lon,loni)
+     call coord_2_int(ph%lat, in_lat, lati)
+     call coord_2_int(time, in_time, ti)
 
-			inds = int( (/loni(2)-1, lati(2)-1, 1, ti(2) /) )
-			call check( nf90_inq_varid(ncid, "PH", tId ) )
-			call check (nf90_get_var(ncid, tId, pert_levels, inds, (/ 4,4,dimlen,1/) ) )
+     inds = int( (/loni(2)-1, lati(2)-1, 1, ti(2) /) )
+     tt = ( ti(2) - ph%ti) + 1
+     if(tt.gt.2)then
+         call grab_grid(ph, ph%ti+1 )
+     end if
+     tt = ( ti(2) - ph%ti) + 1
 
-		   define_levels = (define_levels + pert_levels) / g
-		   do l=1,dimlen
-		   profile(l) = bicubic_interp(define_levels(:,:,l), lati(1), loni(1) )
-		   end do
-		   w%lev = profile
-			exit
-		end if
-	 end do
-	 allocate (ustag_lev(dimlen-1))
+     define_levels = ( ph%base(inds(2):inds(2)+3, inds(2):inds(2)+3, inds(3):inds(3)+3) + ph%grid(inds(2):inds(2)+3, inds(2):inds(2)+3, inds(3):inds(3)+3, tt)) / g
+     do l=1,dimlen
+         profile(l) = bicubic_interp(define_levels(:,:,l), lati(1), loni(1) )
+     end do
+
+     w%lev = profile
 	 ustag_lev =  (profile(2:dimlen) + profile(1:dimlen-1) )/ 2.
 	 u%lev = ustag_lev
 	 v%lev = ustag_lev
+
+	call cpu_time ( t2 )
+    get_levels_time = get_levels_time + (t2-t1)
+
     end subroutine get_levels
 
 
-	real function get4dval(ncid, reqvar, in_time, in_lev, in_lat, in_lon)
+	real function get4dval( reqvar, in_time, in_lev, in_lat, in_lon)
 	real ti(3), levi(3), loni(3), lati(3),  in_time, in_lat, in_lon, in_lev
-	real pro1(4), var(4,4,4)
-	integer inds(4), ninds(4), ncid
+	real pro1(4), var(4,4,4), t1, t2
+	integer inds(4), ninds(4), tt
 	type (wind) reqvar
 	real bicubic_interp, neville_interp
 	ninds = (/4,4,4,1/)
+    call cpu_time ( t1 )
 
     call coord_2_int(time, in_time, ti)
     call coord_2_int(reqvar%lev, in_lev,levi)
     call coord_2_int(reqvar%lon, in_lon,lati)
     call coord_2_int(reqvar%lat, in_lat,loni)
-    if(.not.istat)then
-	return
-    end if
-    inds = int( (/loni(2)-1, lati(2)-1, levi(2)-1, ti(2) /) )
 
-    call check (nf90_get_var(ncid, reqvar%id, var, inds, ninds ) )
+    inds = int( (/loni(2)-1, lati(2)-1, levi(2)-1, ti(2) /) )
+    tt = ( ti(2) - reqvar%ti) + 1
+    if(tt.gt.2)then
+        call grab_grid(reqvar, reqvar%ti+1 )
+    end if
+    tt = ( ti(2) - reqvar%ti) + 1
+
+    var = reqvar%grid(inds(2):inds(2)+3, inds(2):inds(2)+3, inds(3):inds(3)+3, tt)
 
 	!  read in bottom left - bottom right - top left -  topright
 	do i=1,4
@@ -366,6 +386,10 @@
 	if(ti(1).ne.0)then
 !   would also be a istat time.
 	end if
+
+	call cpu_time ( t2 )
+   get4dal_time = get4dal_time + (t2-t1)
+
 
     get4dval = val
     end function get4dval
